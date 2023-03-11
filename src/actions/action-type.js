20feff1b615a4c11dbad7094ce3ab1d4c2766bda
fpaolant <fabiopaolantonio@gmail.com>
2023-03-11 17:53:15 +0100

    login, searchplace, map region view,

--- src/actions/action-type.js
+++ src/actions/action-type.js
@@ -1,6 +1,8 @@
 // App action type
 export const CHANGE_PAGE = 'CHANGE_PAGE';
 export const FIRST_ACCESS = 'FIRST_ACCESS';
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
 
 // Home Search Place
 export const SET_PLACE_NAME = 'SET_PLACE_NAME'
