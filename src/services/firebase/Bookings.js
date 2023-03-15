67849365c824f6097921dac801f1b422eb172835
Fabio <fabiopaolantonio@gmail.com>
2023-03-15 12:47:11 +0100

    added storage city

--- src/services/firebase/Bookings.js
+++ src/services/firebase/Bookings.js
@@ -1,21 +1,21 @@
-import moment from 'moment';
-import { doc, addDoc, collection, getDocs, query, where, orderBy } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where, orderBy } from "firebase/firestore";
 import { db } from './firebase';
 
 
 
 export class Booking {
-    constructor (datetime, days, amount, qty, storageId, storageName, uid) {
+    constructor (datetime, days, amount, qty, storageId, storageName, storageCity, uid) {
         this.datetime = datetime;
         this.days = days;
         this.amount = amount;
         this.qty = qty;
         this.storageId = storageId;
         this.storageName = storageName,
+        this.storageCity = storageCity,
         this.uid = uid;
     }
     toString() {
-        return this.storageName + ', ' 
+        return this.storageName + ', ' + this.storageCity + ', ' 
             + this.datetime + ', ' + this.days + ', ' 
             + this.amount + ', ' + this.qty + ', ' 
             + this.storageId + ', ' + this.uid;
@@ -32,15 +32,14 @@ const BookingConverter = {
             qty: booking.qty,
             storageId: booking.storageId,
             storageName: booking.storageName,
+            storageCity: booking.storageCity,
             uid: booking.uid
         };
     },
     fromFirestore: (snapshot, options) => {
         const data = snapshot.data(options);
         
-        const readableDate = moment.unix(data.datetime.seconds).format("DD/MM/YYYY HH:mm");
-
-        return new Booking(readableDate, data.days, data.amount, data.qty, data.storageId, data.storageName, data.uid);
+        return new Booking(data.datetime, data.days, data.amount, data.qty, data.storageId, data.storageName, data.storageCity, data.uid);
     }
 };
 
--- src/services/firebase/Storages.js
+++ src/services/firebase/Storages.js
@@ -5,19 +5,20 @@ import { db } from './firebase';
 
 
 class Storage {
-    constructor (name, h24, note, latitude, longitude, price ) {
+    constructor (name, h24, note, latitude, longitude, price, city ) {
         this.name = name,
         this.h24 = h24;
         this.note = note;
         this.latitude = latitude;
         this.longitude = longitude;
         this.price = price;
+        this.city = city;
     }
     toString() {
         return this.name + ', ' 
             + this.h24 + ', ' + this.note + ', ' 
             + this.latitude + ', ' + this.longitude
-            + ', ' + this.price;
+            + ', ' + this.price + ', ' + this.city;
     }
 }
 
@@ -29,14 +30,15 @@ const StorageConverter = {
             h24: storage.h24,
             note: storage.note,
             position:  { latitude: storage.latitude, longitude: storage.longitude  },
-            price: storage.price
+            price: storage.price,
+            city: storage.city
         };
     },
     fromFirestore: (snapshot, options) => {
         const data = snapshot.data(options);
         const latitude = data.position.latitude;
         const longitude = data.position.longitude;
-        return new Storage(data.name, data.h24, data.note, latitude, longitude, data.price);
+        return new Storage(data.name, data.h24, data.note, latitude, longitude, data.price, data.city);
     }
 };
 
