d38f881a520a406d4716457b56b2956acb8e740e
fpaolant <fabiopaolantonio@gmail.com>
2023-04-20 12:42:11 +0200

    hidn

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,4 +1,4 @@
-export const GAPIKEY = "AIzaSyBvMJFGVyr4kBfwfkXhVFG7g29YF4bq7Nw";
+export const GAPIKEY = "your-maps-key";
 export const mapDefaultRegion = {"latitude": 42.480561132218696, "latitudeDelta": 12.168153153630918, "longitude": 12.972644393880328, "longitudeDelta": 9.595476896239092};
 export const mapSearchDelta = {"latitudeDelta": 0.32709074386621495, "longitudeDelta": 0.25559712803263324};
 
